c13c54bb1b1940fb762593c92bc3f94e696a50a1
AdhemarA <aahborgesnogueira@gmail.com>
2024-12-15 19:02:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.get( "/hello", function(req,res){
+  res.json({greetings: "Hello, API"});
+} );
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+let multer = require("multer");
+
+app.post( "/api/fileanalyse", multer().single("upfile"), (req,res) =>{
+  let respObj = {};
+  respObj[ "name"] = req.file.originalname;
+  respObj[ "type"] = req.file.mimetype;
+  respObj[ "size"] = req.file.size;
+  res.json({ respObj });
 });
